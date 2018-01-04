7beac116bbd36950ebf4c74405ed7cdeb66bcfe8
Cameron Behdani <cam_b2012@yahoo.com>
2018-01-03 20:45:33 -0500

    clicking cactus changes background color

--- scroll.js
+++ scroll.js
@@ -1,20 +1,41 @@
 $(document).ready(function() {
-    $('#planepic1').click(function() {
+  $('#planepic1').click(function() {
+//        
+//        
+//$(this).rotate({
+//      duration:1000,
+//      angle: 0,
+//      animateTo:30
+//      });
+//       
+//        
+//        
         $("#planepic1").animate({
             top: '+=10vh'
-        }, 1000);
-        
-        
+        }, 1000);    
+//      $(this).rotate({
+//      duration:2000,
+//      angle: 50,
+//      animateTo:-30
+//      });      
+//        
         $("#planepic1").animate({
             top: '-=15vh'
         }, 1000);
-        
-        
+//        
+//        
         $("#planepic1").animate({
             top: '+=5vh'
         }, 1000);
-        
-        
-        
+//        
+//        
+//        
     });
+    
+    
+    $('.cactus_1').click(function() {
+    $("#jumbo1").toggleClass('jumbotronstate1');
+    $("#jumbo1").toggleClass('jumbotronstate2');    
+});
+    
 });
