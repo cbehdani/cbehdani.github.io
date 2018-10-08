5ef7451d00cde2cfdf89d937e952c177c473bb00
cbehdani <cam_b2012@yahoo.com>
2018-10-08 15:30:05 -0400

    fixed bio, road lines, currently working on languages section, project section and smooth scroll

--- SiteJS/scroll.js
+++ SiteJS/scroll.js
@@ -43,33 +43,6 @@ $(document).ready(function() {
     
 });
 
-$(function() {
-      // Select link by id and add click event
-      $('#scrolltest, #mp').click(function() {
 
-        // Animate Scroll to #bottom
-        $('html,body').animate({
-          scrollTop: $("#bodyscroll").offset().top }, // Tell it to scroll to the top #bottom
-          2000 // How long scroll will take in milliseconds
-        );
 
-        // Prevent default behavior of link
-        return false;
-      });
-    });
-
-$(function() {
-      // Select link by id and add click event
-      $('#home').click(function() {
-
-        // Animate Scroll to #bottom
-        $('html,body').animate({
-          scrollTop: '0px'}, // Tell it to scroll to the top #bottom
-          1500 // How long scroll will take in milliseconds
-        );
-
-        // Prevent default behavior of link
-        return false;
-      });
-    });
 
